b49863493684664b52e1bd146973d7c207a469be
belengaratt-pixel <belengaratt@gmail.com>
2026-05-31 22:08:17 -0300

    Update image-trail-demo.tsx

--- components/image-trail-demo.tsx
+++ components/image-trail-demo.tsx
@@ -7,12 +7,12 @@ const ImageTrailDemo = () => {
   const ref = useRef<HTMLDivElement>(null)
 
   const images = [
-    "https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05",
-    "https://images.unsplash.com/photo-1441974231531-c6227db76b6e",
-    "https://images.unsplash.com/photo-1426604966848-d7adac402bff",
-    "https://images.unsplash.com/photo-1472214103451-9374bd1c798e",
-    "https://images.unsplash.com/photo-1469474968028-56623f02e42e",
-    "https://images.unsplash.com/photo-1447752875215-b2761acb3c5d",
+    "https://drive.google.com/file/d/1nnI9CH8MlMzpcf1oHsfKW3bn3dJoygvL/view?usp=sharing",
+    "https://drive.google.com/file/d/1_Ahd98GJCkincD-_xFbZxiAJNgXtX4LG/view?usp=sharing",
+    "https://drive.google.com/file/d/1O8r6SiRQsmcXDBXHbk_nS_0j2p7Q13MP/view?usp=sharing",
+    "https://drive.google.com/file/d/1IPalBK6P25IaQ_mLrM5nzktjH1K2TpiS/view?usp=sharing",
+    "https://drive.google.com/file/d/15InelvBCf2PyKRMVzbgE0xlkXwyJPacS/view?usp=sharing",
+    "https://drive.google.com/file/d/14TV3fkLSZNRD3HLRAomqjFeJOSyPRwYP/view?usp=sharing",
   ].map((url) => `${url}?auto=format&fit=crop&w=300&q=80`)
 
   return (
@@ -22,19 +22,20 @@ const ImageTrailDemo = () => {
           {images.map((url, index) => (
             <div
               key={index}
-              className="flex relative overflow-hidden w-24 h-24 rounded-lg"
-            >
-              <img
-                src={url}
-                alt={`Trail image ${index + 1}`}
-                className="object-cover absolute inset-0 hover:scale-110 transition-transform"
+              <div className="flex relative overflow-hidden rounded-lg">
+  <img
+    src={url}
+    alt={`Trail image ${index + 1}`}
+    className="hover:scale-110 transition-transform"
+  />
+</div>
               />
             </div>
           ))}
         </ImageTrail>
       </div>
       <h1 className="text-7xl md:text-9xl font-bold z-10 select-none bg-clip-text text-transparent bg-gradient-to-r from-neutral-950 to-neutral-500">
-        ALBUMS
+        Belu & Mati
       </h1>
     </div>
   )
